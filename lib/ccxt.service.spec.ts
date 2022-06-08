9cef9f1eac407ba6afaaf610bd3fa4c2c0e93959
fasenderos <fasenderos@gmail.com>
2022-06-08 10:46:31 +0200

    test: test ccxt service 80%

--- lib/ccxt.service.spec.ts
+++ lib/ccxt.service.spec.ts
@@ -39,12 +39,15 @@ describe('CcxtService', () => {
     getExchangeSpy = jest.spyOn(ccxtService as any, 'getExchange');
 
     let loadExchangeMarketsSpy: jest.SpyInstance;
-    loadExchangeMarketsSpy = jest.spyOn(ccxtService as any, 'loadExchangeMarkets');
+    loadExchangeMarketsSpy = jest.spyOn(
+      ccxtService as any,
+      'loadExchangeMarkets',
+    );
 
     const exchangeId = 'binance';
     const options: Partial<ccxt.Exchange> = { verbose: false };
     const client = await ccxtService.getClient(exchangeId, options);
-    
+
     expect(getExchangeSpy).toBeCalledTimes(1);
     expect(getExchangeSpy).toBeCalledWith(exchangeId, options);
 
@@ -53,5 +56,105 @@ describe('CcxtService', () => {
 
     expect(client.id).toBe(exchangeId);
     expect(client.verbose).toBe(options.verbose);
+  });
+});
+
+describe('CcxtService onModuleInit', () => {
+  let ccxtService: CcxtService;
+  let options: CcxtModuleOptions;
+  let module: TestingModule;
+
+  afterEach(async () => {
+    jest.resetAllMocks();
+    await module.close();
+  });
+
+  it('should not load defualtExchanges', async () => {
+    module = await Test.createTestingModule({
+      providers: [
+        CcxtService,
+        {
+          provide: CCXT_MODULE_OPTIONS,
+          useValue: {
+            collection: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+    ccxtService = module.get<CcxtService>(CcxtService);
+
+    let loadExchange: jest.SpyInstance;
+    loadExchange = jest.spyOn(ccxtService as any, 'loadExchange');
+    await ccxtService.onModuleInit();
+    expect(loadExchange).toBeCalledTimes(0);
+    // @ts-ignore
+    expect(ccxtService._clients).toEqual({});
+  });
+
+  it('should load one defualtExchanges', async () => {
+    const exchangeId = 'binance';
+    const ccxtOptions = {
+      rateLimit: 10000,
+    };
+    const verbose = true;
+    const loadMarketsOnStartup = false;
+    module = await Test.createTestingModule({
+      providers: [
+        CcxtService,
+        {
+          provide: CCXT_MODULE_OPTIONS,
+          useValue: {
+            collection: jest.fn(),
+            defaultExchanges: exchangeId,
+            sandboxMode: true,
+            loadMarketsOnStartup,
+            verbose,
+            marketsCacheExpireMs: 0,
+            ccxtOptions,
+          },
+        },
+      ],
+    }).compile();
+    ccxtService = module.get<CcxtService>(CcxtService);
+
+    let loadExchange: jest.SpyInstance;
+    loadExchange = jest.spyOn(ccxtService as any, 'loadExchange');
+
+    await ccxtService.onModuleInit();
+    await ccxtService.getClient(exchangeId, options);
+    expect(loadExchange).toBeCalledTimes(1);
+    expect(loadExchange).toBeCalledWith(exchangeId, ccxtOptions, loadMarketsOnStartup);
+    // @ts-ignore
+    expect(ccxtService._clients[exchangeId].rateLimit).toBe(ccxtOptions.rateLimit);
+    // @ts-ignore
+    expect(ccxtService._clients[exchangeId].verbose).toBe(verbose);
+  });
+
+  it('should load multiple defualtExchanges', async () => {
+    const exchangeId = ['binance', 'ftx'];
+    module = await Test.createTestingModule({
+      providers: [
+        CcxtService,
+        {
+          provide: CCXT_MODULE_OPTIONS,
+          useValue: {
+            collection: jest.fn(),
+            defaultExchanges: exchangeId,
+            verbose: false,
+          },
+        },
+      ],
+    }).compile();
+    ccxtService = module.get<CcxtService>(CcxtService);
+
+    let loadExchange: jest.SpyInstance;
+    loadExchange = jest.spyOn(ccxtService as any, 'loadExchange');
+
+    await ccxtService.onModuleInit();
+    expect(loadExchange).toBeCalledTimes(exchangeId.length);
+    exchangeId.forEach(id => {
+      // @ts-ignore
+      expect(ccxtService._clients[id]).toBeDefined();
+    });
   });
 });
